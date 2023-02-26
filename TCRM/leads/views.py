7f423252a70fc912c08e374d154949f6f81e5332
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-02-26 19:41:04 +0100

    first view

--- TCRM/leads/views.py
+++ TCRM/leads/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def homepage(request):
+    # return HttpResponse("Hello world")
+    return render(request, 'leads/homepage.html')
