bd2c2d797aabd8ffcf5e09a9dd1ca02bc93be551
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-02-26 13:36:26 +0100

    custom user created

--- TCRM/leads/models.py
+++ TCRM/leads/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
 
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField()
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
