15d3e796cdb71546c213ad3f7a8d44ae334aa198
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-02-28 11:20:44 +0100

    lead form added

--- TCRM/leads/urls.py
+++ TCRM/leads/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import homepage, lead_details
+from .views import homepage, lead_details, lead_form
 
 
 app_name = "leads"
@@ -7,6 +7,7 @@ app_name = "leads"
 
 urlpatterns = [
     path('', homepage, name="homepage"),
+    path('form/', lead_form, name="lead_form" ),
     path('<int:pk>/', lead_details, name="lead_details"),
 
 ]
--- TCRM/leads/views.py
+++ TCRM/leads/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lead
+from .forms import LeadForm
 
 # Create your views here.
 
@@ -16,3 +17,11 @@ def lead_details(request, pk):
     details = Lead.objects.get(id=pk)
     context = {"details":details}
     return render(request, 'leadlist.html', context)
+
+
+def lead_form(request):
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+    context = {"form":form}
+    return render(request,'lead_form.html',context)
