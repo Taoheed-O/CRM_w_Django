a546f7a99d0199950b94d875213d4d564455faff
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-02-28 11:35:56 +0100

    code review

--- TCRM/leads/views.py
+++ TCRM/leads/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
 from .forms import LeadForm
 
 # Create your views here.
@@ -23,5 +23,15 @@ def lead_form(request):
     form = LeadForm()
     if request.method == "POST":
         form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(first_name=first_name,
+                                last_name=last_name,
+                                age=age,
+                                agent=agent)
+            return redirect('/')
     context = {"form":form}
     return render(request,'lead_form.html',context)
