6b9a8bc34528e7a204a0ae68892b5207dbc25fe6
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-03-04 16:06:50 +0100

    LoginView class..

--- TCRM/TCRM/urls.py
+++ TCRM/TCRM/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("leads.urls")),
+    path('login/', LoginView.as_view(), name='login'),
 ]
 
 if settings.DEBUG:
