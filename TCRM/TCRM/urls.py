5571916203260013ebc5ba5cc69813b8daf2aeeb
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-03-05 02:32:20 +0100

    log out viewpage...

--- TCRM/TCRM/urls.py
+++ TCRM/TCRM/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("leads.urls")),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
 
 if settings.DEBUG:
