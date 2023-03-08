2371970d8f81492ee9a6ff16e8afd33302ef34be
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-03-08 13:25:09 +0100

    agents list

--- TCRM/agents/views.py
+++ TCRM/agents/views.py
@@ -9,6 +9,7 @@ from leads.models import Agent
 
 class AgentListView(LoginRequiredMixin, generic.ListView):
     template_name = "agents/agent_list.html"
+    context_object_name = "agents"
     
     def get_queryset(self):
         return Agent.objects.all()
